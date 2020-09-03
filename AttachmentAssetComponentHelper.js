1ee4df65e474d26f7bb95c8dfb66c1ecf120fa84
divya maheshwari <50374911+divyachh@users.noreply.github.com>
2020-09-03 15:32:07 +0530

    Update AttachmentAssetComponentHelper.js

--- AttachmentAssetComponentHelper.js
+++ AttachmentAssetComponentHelper.js
@@ -1,20 +1,22 @@
 ({
-    downloadfile : function (component, event, helper){
+    downloadFile : function (component, event, helper){
         
         var actiondownload = component.get("c.loadContentVersion");
         actiondownload.setCallback(this, function(response){
             var state = response.getState();
             if (state === "SUCCESS") {
                 //component.set("v.base64Value", response.getReturnValue());
-               var xyz =response.getReturnValue();
-               component.set("v.base64Value", xyz[0]);
-               component.set("v.contentTyp", xyz[1]);
+               var response =response.getReturnValue();
+               //component.set("v.base64Value", response[0]);
+               //component.set("v.contentType", response[1]);
+               console.log(JSON.stringify(response.getReturnValue())); 
+               
             }
             else {
                 console.log("Failed with state: " + state);
             }
         });
-        $A.enqueueAction(action);
+        $A.enqueueAction(actiondownload);
     }
     
     
